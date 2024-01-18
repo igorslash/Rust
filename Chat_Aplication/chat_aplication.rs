2d871e446f7e23438e05c525b31507fe2bacd636
Igor <58830706+igorslash@users.noreply.github.com>
2024-01-18 15:59:32 +0300

    Update chat_aplication.rs

--- Chat_Aplication/chat_aplication.rs
+++ Chat_Aplication/chat_aplication.rs
@@ -10,15 +10,20 @@ const MSG_SIZE: usize = 32;
 
 fn main() {
     // create a server
+    
     let server = TcpListener::bind(LOCAL)
         .expect("Listener failed to bind");
+    
     // set server to non-blocking
     server.set_nonblocking(true).expect("Failed to initialize non-blocking");
+    
     // create a vector of clients
     let mut clients = vec![];
+    
     // create a channel for messages
     let (sender, receiver) =
         mpsc::channel::<String>();
+    
     loop {
         // handle new connections
         if let Ok((mut socket, addr)) = server.accept() {
@@ -26,9 +31,11 @@ fn main() {
             let tx = &sender;
             clients.push(socket.try_clone()
                 .expect("Failed to clone client"));
+           
             // send message to client
             thread::spawn(move || loop {
                 let mut buff = vec![0; MSG_SIZE];
+                
                 // read message from client
                 match socket.read_exact(&mut buff) {
                     Ok(_) => {
@@ -42,6 +49,7 @@ fn main() {
                         println!("{}: {:?}", addr, msg);
                         tx.send(msg).expect("Failed to send msg");
                     },
+                    
                     // handle error
                     Err(ref err) if err.kind() ==
                         ErrorKind::WouldBlock => (),
