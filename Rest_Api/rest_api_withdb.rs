6260f32c835dfee2fb59daf9df3fc56727613d4f
Igor <58830706+igorslash@users.noreply.github.com>
2024-03-01 21:43:03 +0300

    Add files via upload

--- Rest_Api/rest_api_withdb.rs
+++ Rest_Api/rest_api_withdb.rs
@@ -23,6 +23,12 @@ async fn main() {
         .and(warp::query::<u32>())
         .and_then(get_user_by_id_handler(pool.clone(), 1));
 
+    let delete_user = warp::path("deleteUser")
+        .and(warp::path::end())
+        .and(warp::get())
+        .and(warp::query::<u32>())
+        .and_then(delete_user_by_id_handler(pool.clone(), 2));
+
     let routes = users.or(get_user_name);
 
     warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
@@ -49,6 +55,35 @@ async fn get_users_handler(pool: sqlx::PgPool) -> Result<impl Reply, Rejection> 
 async fn get_user_by_id_handler(pool: sqlx::PgPool,
                                 id: u32) -> Result<impl Reply, Rejection> {
     let user = sqlx::query_as::<_, User>("SELECT * FROM users WHERE id = $1")
+        .bind(id as i32)
+        .fetch_optional(&pool)
+        .await
+        .map_err(|e| {
+            eprintln!("Database error: {}", e);
+            warp::reject()
+        })?;
+    match user {
+        Some(u) => Ok(warp::reply::json(&u)),
+        None => Ok(warp::reply::json(&"User not found")),
+    }
+}
+async fn add_user(pool: sqlx::PgPool, user: User) -> Result<impl
+warp::Reply, Rejection> {
+    let user = sqlx::query_as::<_, User>("INSERT INTO \
+    users (username, email) VALUES ($1, $2) RETURNING *")
+        .bind(user.username)
+        .bind(user.email)
+        .fetch_one(&pool)
+        .await
+        .map_err(|e| {
+            eprintln!("Database error: {}", e);
+            warp::reject()
+        })?;
+    Ok(warp::reply::json(&user))
+}
+async fn delete_user_by_id_handler(pool: sqlx::PgPool,
+                                    id: u32) -> Result<impl Reply, Rejection> {
+    let user = sqlx::query_as::<_, User>("DELETE FROM users WHERE id = $1")
         .bind(id as i32)
         .fetch_optional(&pool)
         .await
